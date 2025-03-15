758940fa1c7fcf64db8a6f091e28a7ce1cb003e4
Federico Giorgino <59868572+federicogiorgino@users.noreply.github.com>
2025-03-15 15:53:15 +0100

    chore: initialize git repo

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div>content</div>;
+}
+
+export default HomePage;
---
+++ next.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreBuildErrors: true },
+};
+
+export default nextConfig;
